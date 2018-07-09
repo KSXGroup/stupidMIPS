0f21c7ce8b7ca98ec488c3293368e576db2a997b
KSXGroup <1370058225@qq.com>
2018-07-09 10:59:15 +0800

    LENGTHEN HISTORY

--- mips_define.hpp
+++ mips_define.hpp
@@ -8,6 +8,7 @@
 //#define TEXT_DEBUG
 //#define PARSED_PROGRAM_DEBUG
 //#define REG_DEBUG
+//#define PREDICTION_DEBUG
 #include <iostream>
 #include <fstream>
 #include <map>
@@ -20,7 +21,7 @@ typedef unsigned char BYTE;
 typedef unsigned int OFFSETTYPE;
 const unsigned int MAX_MEMORY_SIZE = 1024 * 1024 * 4;
 const unsigned int BRANCH_PREDICTOR_CACHE = 2000;
-const unsigned char PREDICT_BYTE = 4;
+const unsigned char PREDICT_BYTE = 8;
 const BYTE TYPE_R = 0;
 const BYTE TYPE_L = 1;
 const BYTE TYPE_J = 2;
